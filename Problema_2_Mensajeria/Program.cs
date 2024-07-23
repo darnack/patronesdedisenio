eb4ee7b6c2cc0eff2c03c535a93a8d883ad1eaad
Daniel Santiago <daniel.melos98@gmail.com>
2024-07-22 21:07:50 -0500

    Se crea proyecto para el ejercicio 2

---
+++ Problema_2_Mensajeria/Program.cs
@@ -0,0 +1,23 @@
+﻿using Problema_2_Mensajeria.Dispositivos;
+using Problema_2_Mensajeria.Users;
+
+Console.WriteLine("Sistema de mensajería.");
+
+IDispositivo ObjComputador = new Dispositivo("Computador");
+IDispositivo ObjCelular = new Dispositivo("Celular");
+IDispositivo ObjAsistenteHogar = new Dispositivo("Asistente de hogar");
+
+var ObjUsuario1 = new Usuario("Daniel");
+ObjUsuario1.AgregarDispositivo(ObjComputador);
+ObjUsuario1.AgregarDispositivo(ObjCelular);
+
+var ObjUsuario2 = new Usuario("Jorge");
+ObjUsuario2.AgregarDispositivo(ObjAsistenteHogar);
+
+
+ObjUsuario1.Notificar("El producto Alfa ya se encuentra disponible!");
+ObjUsuario2.Notificar("En 2 días se ha programado el mantenimiento de tu vehículo.");
+
+ObjUsuario1.EliminarDispositivo(ObjComputador);
+
+
---
+++ Problema_2_Mensajeria/README.md
@@ -0,0 +1,5 @@
+# Ejercicio 2 patrón de diseño
+
+## Patrón seleccionado
+- **Observer**
+    -  Se seleccionó este patrón, ya que da una solución sencilla para la actualización de distintos objetos al mismo tiempo bajo una misma característica, en este caso, estar asignados a un mismo usuario, esto permite que dada una notificación para determinado usuario, se genere una acción sobre cada uno de sus dispositivos, adicionalmente, se tiene control sobre la agregación y eliminación de dispositivos por usuario.
